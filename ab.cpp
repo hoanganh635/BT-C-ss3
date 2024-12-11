68406e9921a5ee2b2c3f1d01e04c67f8f1fd1eda
hoanganh635 <hoanganhnv1997@gmail.com>
2024-12-11 21:51:57 +0700

    Add files via upload

---
+++ ab.cpp
@@ -0,0 +1,11 @@
+int main() {
+    int C;
+    float F;
+    printf("moi ban nhap do C\n");
+    scanf("%d", &C);
+    printf("do c la %d",C);
+    F = (C * 1.8) + 32;
+    printf("do F la %.1f",F);
+    return 0;
+}
+
